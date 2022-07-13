7870fe5d80e9faebd1fa936381e5ed340b0f5367
kpputhuveetil <kpputhuveetil@vcu.edu>
2022-07-13 14:17:28 -0400

    added checkpoint loader

--- gnn_train_test_new.py
+++ gnn_train_test_new.py
@@ -1,6 +1,6 @@
 #%%
 # from _typeshed import NoneType
-import os, multiprocessing
+import os, multiprocessing, glob
 import os.path as osp
 import numpy as np
 import json
@@ -13,8 +13,8 @@ from torch_geometric.data import Data
 import time
 from pathlib import Path
 
-# from bm_dataset import BMDataset
-from bm_dataset_3D import BMDataset
+from bm_dataset import BMDataset
+# from bm_dataset_3D import BMDataset
 # from bm_dataset_no_edge_attr import BMDataset
 from models_graph_res import GNNModel
 from types import SimpleNamespace
@@ -25,7 +25,103 @@ import configparser
 
 #%%
 class GNN_Train_Test():
-    def __init__(self, save_dir, cuda_device, dataset=None, train_test=None, proc_layers=None, num_images=None, epochs=None, learning_rate=None, seed=None, batch_size=None, num_workers=None, model_descirption=None):
+    def __init__(self, device) :
+        # pass
+        # cuda_device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.device = torch.device(device)
+        print("Device:", self.device)
+
+    def load_model_from_checkpoint(self, model_dir, model_checkpoint_number=None, batch_size=None, num_workers=None, epochs =None, new_checkpoint_dir=None):
+        checkpoint_dir = osp.join(model_dir, 'checkpoints')
+        
+        # data_dir = osp.join(path, root, 'raw/*.pkl')
+        if model_checkpoint_number==None:
+            all_checkpoints = glob.glob(osp.join(checkpoint_dir, '*.pth'))
+            checkpoint_path = sorted(all_checkpoints)[-1] # pick last checkpoint
+            model_checkpoint_number = len(all_checkpoints)-1
+            # print(model_checkpoint_number)
+        else:
+            checkpoint_path = osp.join(checkpoint_dir, f'model_{model_checkpoint_number}.pth')
+
+        self.model_checkpoint_number = model_checkpoint_number #! add code to generate this when you first make the model too
+
+        config = configparser.ConfigParser()
+        config.read(osp.join(model_dir, 'config.ini'))
+
+        model_config = config['Model']
+        epochs = epochs if epochs != None else int(model_config['epochs'])
+        proc_layers = int(model_config['proc_layer_num'])
+        learning_rate = float(model_config['learning_rate'])
+        seed = int(model_config['seed'])
+        global_size = int(model_config['global_size'])
+        output_size = int(model_config['output_size'])
+        node_dim = int(model_config['node_dim'])
+        edge_dim = int(model_config['edge_dim'])
+
+        self.args = SimpleNamespace(
+                    seed=seed,
+                    learning_rate=learning_rate,
+                    epoch = epochs,
+                    proc_layer_num=proc_layers, 
+                    global_size=global_size,
+                    output_size=output_size,
+                    node_dim=node_dim,
+                    edge_dim=edge_dim)
+
+        self.batch_size = batch_size if batch_size != None else int(model_config['batch_size'])
+        self.num_workers = num_workers if num_workers != None else int(model_config['num_workers'])
+        #! CHANGE THIS BACK IS POSSIBLE
+        self.num_workers = 0
+
+        self.model = GNNModel(self.args, self.args.proc_layer_num, self.args.global_size, self.args.output_size)
+        self.model.to(self.device)
+        self.model.load_state_dict(torch.load(checkpoint_path)['model'])
+        self.model_criterion = torch.nn.MSELoss()
+        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=self.args.learning_rate)
+        self.scheduler = ReduceLROnPlateau(self.optimizer, 'min', factor=0.8, patience=3, verbose=True)
+
+        # # self.checkpoints_dir = osp.join(new_checkpoint_dir,'checkpoints')
+        # # Path(self.checkpoints_dir).mkdir(parents=True, exist_ok=True)
+        # self.writer_dir = osp.join(new_checkpoint_dir, 'runs')
+        # self.writer = SummaryWriter(self.writer_dir)
+
+    def set_initial_dataset(self, dataset, train_test=None, num_images=None):
+        # if you pass a single float for train_test, interpret as the ratio of train:test points to use from the entire dataset
+        if isinstance(train_test, float):
+            train_test_ratio = train_test
+            train_len = round(len(dataset)*train_test_ratio)
+            test_len = -1
+        # if you pass a tuple for train_test, interpret as the number of train points and test points to select from the entire dataset of points
+        elif isinstance(train_test, tuple): 
+            train_len = train_test[0]
+            test_len = train_test[0] + train_test[1]
+        self.TRAIN_DATASET = dataset[:train_len]
+        self.TEST_DATASET = dataset[train_len:test_len]
+
+        # TRAIN_DATASET = TEST_DATASET = [dataset[0]]
+        # batch_size = num_workers = 1
+        if dataset is not None:
+            self.trainDataLoader = torch_geometric.data.DataLoader(self.TRAIN_DATASET, batch_size=self.batch_size, shuffle=True, num_workers=self.num_workers,
+                                                            pin_memory=True, drop_last=False)
+            self.testDataLoader = torch_geometric.data.DataLoader(self.TEST_DATASET, batch_size=self.batch_size, shuffle=False, num_workers=self.num_workers,
+                                                            pin_memory=True, drop_last=False)
+            self.imageDataLoader = torch_geometric.data.DataLoader(self.TEST_DATASET[0:num_images], batch_size=1, shuffle=False, num_workers=1,
+                                                            pin_memory=True, drop_last=False)
+
+            print("The number of training data is: %d" % len(self.TRAIN_DATASET))
+            print("The number of test data is: %d" % len(self.TEST_DATASET))
+            print("The number of image data is: %d" % num_images)
+    
+    def add_to_train_set(self, dataset):
+
+        self.TRAIN_DATASET = torch.utils.data.ConcatDataset([self.TRAIN_DATASET, dataset])
+        self.trainDataLoader = torch_geometric.data.DataLoader(self.TRAIN_DATASET, batch_size=self.batch_size, shuffle=True, num_workers=self.num_workers,
+                                                            pin_memory=True, drop_last=False)
+        # self.batch_size = len(dataset)
+        self.batch_size = 1
+
+
+    def create_new_model(self, save_dir, cuda_device, dataset=None, train_test=None, proc_layers=None, num_images=None, epochs=None, learning_rate=None, seed=None, batch_size=None, num_workers=None, model_descirption=None):
         torch.cuda.empty_cache()
         # print(f"TEST: edge threshold = {edge_thres}, action to {action_to_node} nodes, processing layers = {proc_layers}")
         self.model_id = f"{model_descirption}_epochs={epochs}_batch={batch_size}_workers={num_workers}_{round(time.time())}"
@@ -42,11 +138,6 @@ class GNN_Train_Test():
             torch.cuda.manual_seed_all(seed)
         np.random.seed(seed)
 
-
-        # device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-        self.device = torch.device(cuda_device)
-        print("CUDA Device:", self.device)
-
         # dataset = dataset.shuffle()
 
         # if you pass a single float for train_test, interpret as the ratio of train:test points to use from the entire dataset
@@ -59,6 +150,7 @@ class GNN_Train_Test():
             train_len = train_test[0]
             test_len = train_test[0] + train_test[1]
         TRAIN_DATASET = dataset[:train_len]
+        self.all_train_sets = [TRAIN_DATASET]
         TEST_DATASET = dataset[train_len:test_len]
         # TEST_DATASET = dataset[-1* train_test[1]:]
 
@@ -73,11 +165,11 @@ class GNN_Train_Test():
         # batch_size = num_workers = 1
         if dataset is not None:
             self.trainDataLoader = torch_geometric.data.DataLoader(TRAIN_DATASET, batch_size=self.batch_size, shuffle=True, num_workers=self.num_workers,
-                                                            pin_memory=True, drop_last=True)
+                                                            pin_memory=True, drop_last=False)
             self.testDataLoader = torch_geometric.data.DataLoader(TEST_DATASET, batch_size=self.batch_size, shuffle=False, num_workers=self.num_workers,
-                                                            pin_memory=True, drop_last=True)
+                                                            pin_memory=True, drop_last=False)
             self.imageDataLoader = torch_geometric.data.DataLoader(TEST_DATASET[0:num_images], batch_size=1, shuffle=False, num_workers=1,
-                                                            pin_memory=True, drop_last=True)
+                                                            pin_memory=True, drop_last=False)
 
             print("The number of training data is: %d" % len(TRAIN_DATASET))
             print("The number of test data is: %d" % len(TEST_DATASET))
@@ -117,7 +209,8 @@ class GNN_Train_Test():
             'voxel_size':dataset.voxel_size,
             'subsample':dataset.subsample,
             'edge_threshold':dataset.edge_threshold,
-            'action_to_all':dataset.action_to_all}
+            'action_to_all':dataset.action_to_all,
+            'shuffle':False}
         config['Model'] = {
             'seed':self.args.seed,
             'learning_rate':self.args.learning_rate,
@@ -322,61 +415,117 @@ class GNN_Train_Test():
 
         return fig
 
-
-# %%
+# # %%
 # dataset = BMDataset(
-#     root='/home/kpputhuveetil/git/vBM-GNNdev/gnn_new_data/3D_dataset', 
-#     description='50k_samples_3D',
+#     root='/home/kpputhuveetil/git/vBM-GNNdev/gnn_blanket_var_data', 
+#     description='blanket_var',
 #     voxel_size=0.05, 
 #     edge_threshold=0.06, 
 #     action_to_all=True, 
 #     testing=False)
-dataset = BMDataset(
-    root='/home/kpputhuveetil/git/vBM-GNNdev/gnn_new_data/3D_dataset', 
-    description='50k_samples_3D',
-    voxel_size=0.05, 
-    edge_threshold=0.07, 
-    action_to_all=True, 
-    testing=False)
-save_dir = osp.abspath(os.path.join(os.getcwd(), os.pardir, 'trained_models'))
-model_description = 'train10k_3D'
-cuda_device = 'cuda:0'
-train_test = (10000, 100)
-num_images = 100
-epochs = 250
-proc_layers = 4
-learning_rate = 1e-4
-seed = 1001
-batch_size = 100
-num_workers = 4
 
-gnn_train_test = GNN_Train_Test(
-    save_dir, cuda_device, dataset, train_test, 
-    proc_layers, num_images, epochs, learning_rate, seed, batch_size, num_workers,
-    model_description)
+# # dataset = BMDataset(
+# #     root='/home/kpputhuveetil/git/vBM-GNNdev/gnn_high_pose_var_data', 
+# #     description='high_pose_var',
+# #     voxel_size=0.05, 
+# #     edge_threshold=0.06, 
+# #     action_to_all=True, 
+# #     testing=False)
 
-#%%
-gnn_train_test.train()
+# # dataset = BMDataset(
+# #     root='/home/kpputhuveetil/git/vBM-GNNdev/gnn_new_data/3D_dataset', 
+# #     description='50k_samples_3D',
+# #     voxel_size=0.05, 
+# #     edge_threshold=0.06, 
+# #     action_to_all=True, 
+# #     testing=False)
+# # dataset = BMDataset(
+# #     root='/home/kpputhuveetil/git/vBM-GNNdev/gnn_new_data/3D_dataset', 
+# #     description='50k_samples_3D',
+# #     voxel_size=0.05, 
+# #     edge_threshold=0.07, 
+# #     action_to_all=True, 
+# #     testing=False)
+# save_dir = osp.abspath(os.path.join(os.getcwd(), os.pardir, 'trained_models'))
+# model_description = 'blanket_var_10k'
+# cuda_device = 'cuda:0'
+# train_test = (10000, 100)
+# num_images = 100
+# epochs = 250
+# proc_layers = 4
+# learning_rate = 1e-4
+# seed = 1001
+# batch_size = 100
+# num_workers = 4
+
+# gnn_train_test = GNN_Train_Test(
+#     save_dir, cuda_device, dataset, train_test, 
+#     proc_layers, num_images, epochs, learning_rate, seed, batch_size, num_workers,
+#     model_description)
 
 # #%%
-# checkpoint = '/home/kpputhuveetil/git/vBM-GNNdev/trained_models/train10k_epochs=250_batch=100_workers=4_1646202554/checkpoints'
-# # gnn_train_test.evaluate(gnn_train_test.checkpoints_dir)
-# # gnn_train_test.evaluate(checkpoint)
+# gnn_train_test.train()
 
-# checkpoint2 = '/home/kpputhuveetil/git/vBM-GNNdev/trained_models/50ktest/checkpoints'
-# # gnn_train_test.evaluate(checkpoint2)
+# # #%%
+# # checkpoint = '/home/kpputhuveetil/git/vBM-GNNdev/trained_models/train10k_epochs=250_batch=100_workers=4_1646202554/checkpoints'
+# # # gnn_train_test.evaluate(gnn_train_test.checkpoints_dir)
+# # # gnn_train_test.evaluate(checkpoint)
 
-# checkpoint3 = '/home/kpputhuveetil/git/vBM-GNNdev/trained_models/9ktest/checkpoints'
+# # checkpoint2 = '/home/kpputhuveetil/git/vBM-GNNdev/trained_models/50ktest/checkpoints'
+# # # gnn_train_test.evaluate(checkpoint2)
 
-#%%
-model_path = '/home/kpputhuveetil/git/vBM-GNNdev/trained_models/train10k_3D_epochs=250_batch=100_workers=4_1646464130'
-gnn_train_test.evaluate(osp.join(model_path,"checkpoints"))
-gnn_train_test.evaluate_images(osp.join(model_path,"checkpoints"), osp.join(model_path,"runs"))
+# # checkpoint3 = '/home/kpputhuveetil/git/vBM-GNNdev/trained_models/9ktest/checkpoints'
 
-#%%
-# gnn_train_test.evaluate_images(checkpoint, "/home/kpputhuveetil/git/vBM-GNNdev/trained_models/train10k_epochs=250_batch=100_workers=4_1646202554/runs")
-# gnn_train_test.evaluate_images(checkpoint2, "/home/kpputhuveetil/git/vBM-GNNdev/trained_models/50ktest/runs")
-# gnn_train_test.evaluate_images(checkpoint3, "/home/kpputhuveetil/git/vBM-GNNdev/trained_models/test/runs")
+# #%%
+# # model_path = '/home/kpputhuveetil/git/vBM-GNNdev/trained_models/train10k_3D_epochs=250_batch=100_workers=4_1646464130'
+# # model_path = '/home/kpputhuveetil/git/vBM-GNNdev/trained_models/high_pose_var_10k_epochs=250_batch=100_workers=4_1647288217'
+# model_path = '/home/kpputhuveetil/git/vBM-GNNdev/trained_models/blanket_var_10k_epochs=250_batch=100_workers=4_1647497242'
+# gnn_train_test.evaluate(osp.join(model_path,"checkpoints"))
+# gnn_train_test.evaluate_images(osp.join(model_path,"checkpoints"), osp.join(model_path,"runs"))
 
-# %%
-# gnn_train_test.evaluate_images(checkpoint3, "/home/kpputhuveetil/git/vBM-GNNdev/trained_models/test/runs")
+# #%%
+# # gnn_train_test.evaluate_images(checkpoint, "/home/kpputhuveetil/git/vBM-GNNdev/trained_models/train10k_epochs=250_batch=100_workers=4_1646202554/runs")
+# # gnn_train_test.evaluate_images(checkpoint2, "/home/kpputhuveetil/git/vBM-GNNdev/trained_models/50ktest/runs")
+# # gnn_train_test.evaluate_images(checkpoint3, "/home/kpputhuveetil/git/vBM-GNNdev/trained_models/test/runs")
+
+# # %%
+# # gnn_train_test.evaluate_images(checkpoint3, "/home/kpputhuveetil/git/vBM-GNNdev/trained_models/test/runs")
+
+
+
+
+# # %%
+# checkpoint = '/home/kpputhuveetil/git/vBM-GNNdev/trained_models/train10k_cont_learn_epochs=250_batch=100_workers=4_1646202554'
+# cont_learning_checkpoint = osp.join(checkpoint,'cont_learning_data')
+# iter_checkpoint = osp.join(cont_learning_checkpoint,'iteration_0')
+# #%%
+# new_dataset = BMDataset(
+#     root= iter_checkpoint, 
+#     description='iter_0_processed',
+#     voxel_size=0.05, 
+#     edge_threshold=0.06, 
+#     action_to_all=True,
+#     use_cma_data=True,
+#     testing=False)
+
+# #%%
+# gnn_train_test = GNN_Train_Test('cuda:0')
+# gnn_train_test.load_model_from_checkpoint(checkpoint, epochs=25, new_checkpoint_dir=iter_checkpoint)
+
+# #%%
+# orig_dataset = BMDataset(
+#     root='/home/kpputhuveetil/git/vBM-GNNdev/gnn_new_data', 
+#     description='50k_samples',
+#     voxel_size=0.05, 
+#     edge_threshold=0.06, 
+#     action_to_all=True, 
+#     testing=False)
+# train_test = (10000, 100)
+# num_images = 100
+
+# # gnn_train_test.set_initial_dataset(new_dataset, train_test=train_test, num_images=num_images)
+# #%%
+# gnn_train_test.add_to_train_set(new_dataset)
+# #%%
+# gnn_train_test.train()
+# # %%
